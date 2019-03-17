9b10ac6e0e1eb3a7fa1ef26a78daabb81af68cc0
Aaron <coder@wiredprairie.us>
2019-03-17 16:02:02 -0500

    Working in prosemirror

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,76 +1,91 @@
-import Quill, { QuillOptionsStatic, RangeStatic, Sources } from "quill";
-import Delta from "quill-delta";
-import "quill/dist/quill.snow.css";
+import { exampleSetup } from "prosemirror-example-setup";
+import "prosemirror-menu/style/menu.css";
+import { DOMParser, Schema } from "prosemirror-model";
+import { EditorState, Transaction } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import "prosemirror-view/style/prosemirror.css";
 import React from "react";
 import "./Editor.scss";
-import toMarkdown from "./utilities/quill-to-markdown";
 
 interface Props {
 	plainTextOnly?: boolean;
 }
 
 interface State {
-	changeLength: number;
+	contentLength: number;
 	text: string;
 	focused: boolean;
+	editorState?: EditorState;
 }
+
+// const mySchema = new Schema({
+// 	nodes: addListNodes(schema.spec.nodes as any, "paragraph block*", "block"),
+// 	marks: schema.spec.marks
+// });
+const textOnlySchema = new Schema({
+	nodes: {
+		text: {},
+		doc: { content: "text*" }
+	}
+});
+
+const mySchema = new Schema({
+	nodes: {
+		text: {},
+		doc: { content: "text*" }
+	}
+});
 
 export default class Editor extends React.Component<Props, State> {
 	static defaultProps: Props = {
 		plainTextOnly: true
 	};
 
-	private _quillHostElement!: HTMLDivElement | null;
-	private _quill?: Quill;
-	private _quillStandardCssNames: string = "";
+	private _viewHostElement!: HTMLDivElement | null;
+	private _viewContentElement!: HTMLDivElement | null;
+
+	private _view?: EditorView;
+	private _editorState?: EditorState;
 
 	constructor(props: Props) {
 		super(props);
 		this.state = {
 			text: "",
-			changeLength: 0,
+			contentLength: 0,
 			focused: false
 		};
 		this.rebinders();
 	}
 
 	private rebinders() {
-		this.onQuillTextChange = this.onQuillTextChange.bind(this);
-		this.onQuillSelectionChange = this.onQuillSelectionChange.bind(this);
-		this.onQuillFocus = this.onQuillFocus.bind(this);
-		this.onQuillBlur = this.onQuillBlur.bind(this);
-		this.onQuillKeyUp = this.onQuillKeyUp.bind(this);
-		this.onQuillMouseUp = this.onQuillMouseUp.bind(this);
-		this.onQuillMouseDown = this.onQuillMouseDown.bind(this);
-		this.onQuillMouseLeave = this.onQuillMouseLeave.bind(this);
-		this.onQuillTouchEnd = this.onQuillTouchEnd.bind(this);
+		//this.editorViewOnDispatchTransaction = this.editorViewOnDispatchTransaction.bind(this);
 	}
 
 	componentDidMount() {
-		if (this._quillHostElement) {
-			const options: QuillOptionsStatic = {
-				modules: { ...this.getToolbar() },
-				theme: "snow",
-				placeholder: "Compose here"
-			};
-			if (this.props.plainTextOnly) {
-				options.formats = [];
-			}
+		if (this._viewHostElement && this._viewContentElement) {
+			const state = EditorState.create({
+				doc: DOMParser.fromSchema(this.props.plainTextOnly ? textOnlySchema : mySchema).parse(
+					this._viewContentElement
+				),
+				plugins: exampleSetup({ schema: mySchema })
+			});
+			this._view = new EditorView(this._viewHostElement, {
+				state,
+				dispatchTransaction: (transaction: Transaction) => {
+					this.editorViewOnDispatchTransaction(transaction);
+				}
+			});
 
-			const quill = new Quill(this._quillHostElement, options);
-			this._quill = quill;
-			// rather than hardcoding the Quill Element CSS class names, grab them after they've
-			// been set. they'll be used later in the control
-			this._quillStandardCssNames = this._quillHostElement.getAttribute("class") || "";
-
-			this.attachQuillEvents(quill);
+			this.setState({
+				editorState: state
+			});
 		}
 	}
 
 	componentWillUnmount() {
-		if (this._quill) {
-			this._quill.disable();
-			this.detachQuillEvents(this._quill);
+		if (this._view) {
+			this._view.destroy();
+			this._view = undefined;
 		}
 	}
 
@@ -80,80 +95,35 @@ export default class Editor extends React.Component<Props, State> {
 		return (
 			<>
 				<div
-					className={`Editor ${this._quillStandardCssNames}${focused ? " infocus is-focused" : ""} `}
+					className={`${focused ? " infocus is-focused" : ""} `}
 					ref={r => {
-						this._quillHostElement = r;
+						this._viewHostElement = r;
 					}}
 				/>
-				<div>{this.state.changeLength}</div>
+				<div
+					style={{
+						display: "none"
+					}}
+					ref={r => {
+						this._viewContentElement = r;
+					}}
+				/>
+				<div>{this.state.contentLength}</div>
 				<pre>{text}</pre>
 			</>
 		);
 	}
 
-	private getToolbar() {
-		if (this.props.plainTextOnly) {
-			// it is necessary to return null to indicate no toolbar
-			return {
-				toolbar: null
-			};
+	private editorViewOnDispatchTransaction(tr: Transaction) {
+		if (this.state.editorState) {
+			this.setState(state => {
+				const editorState = state.editorState!.apply(tr);
+				this._view!.updateState(editorState);
+				return {
+					editorState,
+					contentLength: tr.doc.content.size
+				};
+			});
 		}
-		return {
-			toolbar: [
-				[{ header: [1, 2, false] }],
-				["bold", "italic", "strike", "blockquote"],
-				[{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
-				["link", "image"],
-				["clean"]
-			]
-		};
-	}
-
-	private onQuillKeyUp(ev: KeyboardEvent) {}
-	private onQuillMouseUp(ev: MouseEvent) {}
-	private onQuillMouseDown(ev: MouseEvent) {}
-	private onQuillMouseLeave(ev: MouseEvent) {}
-	private onQuillTouchEnd(ev: TouchEvent) {}
-	private onQuillFocus(ev: FocusEvent) {
-		this.setState({ focused: true });
-	}
-	private onQuillBlur(ev: FocusEvent) {
-		this.setState({ focused: false });
-	}
-
-	private onQuillSelectionChange(range: RangeStatic, oldRange: RangeStatic, source: Sources): void {}
-
-	private onQuillTextChange(delta: Delta, oldContents: Delta, source: String) {
-		this.setState({
-			changeLength: delta.changeLength(),
-			text: this._quill ? toMarkdown(this._quill.getContents()) : ""
-		});
-	}
-
-	private detachQuillEvents(quill: Quill): void {
-		quill.off("text-change", this.onQuillTextChange);
-		quill.off("selection-change", this.onQuillSelectionChange);
-		const host = quill.root;
-		host.removeEventListener("focus", this.onQuillFocus);
-		host.removeEventListener("blur", this.onQuillBlur);
-		host.removeEventListener("keyup", this.onQuillKeyUp);
-		host.removeEventListener("mouseup", this.onQuillMouseUp);
-		host.removeEventListener("mouseleave", this.onQuillMouseLeave);
-		host.removeEventListener("mousedown", this.onQuillMouseDown);
-		host.removeEventListener("touchend", this.onQuillTouchEnd);
-	}
-
-	private attachQuillEvents(quill: Quill): void {
-		quill.on("text-change", this.onQuillTextChange);
-		quill.on("selection-change", this.onQuillSelectionChange);
-
-		const host = quill.root;
-		host.addEventListener("focus", this.onQuillFocus);
-		host.addEventListener("blur", this.onQuillBlur);
-		host.addEventListener("keyup", this.onQuillKeyUp);
-		host.addEventListener("mouseup", this.onQuillMouseUp);
-		host.addEventListener("mouseleave", this.onQuillMouseLeave);
-		host.addEventListener("mousedown", this.onQuillMouseDown);
-		host.addEventListener("touchend", this.onQuillTouchEnd);
 	}
 }
---
+++ types/prosemirror-example-setup.d.ts
@@ -0,0 +1,6 @@
+declare module "prosemirror-example-setup" {
+	export function buildInputRules(schema: any): any;
+	export function buildKeymap(schema: any, mapKeys: any): any;
+	export function buildMenuItems(schema: any): any;
+	export function exampleSetup(options: any): any;
+}
